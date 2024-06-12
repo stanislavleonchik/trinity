0fc0f5ef2479c38064c87ca0eb5e66970ed5b9f5
Stanislav Leonchik <leonstas1@mail.ru>
2024-06-13 01:03:37 +0300

    Update stylesheet

--- web/src/App.js
+++ web/src/App.js
@@ -31,14 +31,17 @@ const UploadFile = ({ onResult }) => {
     };
 
     return (
-        <div className="upload-section">
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleFileUpload}>Upload File</button>
+        <div className="form-section">
+            <h3>Upload File</h3>
+            <div className="form-group">
+                <input type="file" onChange={handleFileChange} className="form-input-file"/>
+                <button onClick={handleFileUpload} className="form-button">Upload File</button>
+            </div>
         </div>
     );
 };
 
-const EnterUrl = ({ onResult }) => {
+const EnterUrl = ({onResult}) => {
     const [url, setUrl] = useState('');
 
     const handleUrlChange = (e) => {
@@ -48,7 +51,7 @@ const EnterUrl = ({ onResult }) => {
     const handleUrlSubmit = async () => {
         try {
             const response = await axios.get(`${backendAddress}/web`, {
-                params: { url },
+                params: {url},
             });
             onResult(response.data);
         } catch (error) {
@@ -57,9 +60,18 @@ const EnterUrl = ({ onResult }) => {
     };
 
     return (
-        <div className="url-section">
-            <input type="text" placeholder="URL of article" value={url} onChange={handleUrlChange} />
-            <button onClick={handleUrlSubmit}>Submit URL</button>
+        <div className="form-section">
+            <h3>Enter URL</h3>
+            <div className="form-group">
+                <input
+                    type="text"
+                    placeholder="URL of article"
+                    value={url}
+                    onChange={handleUrlChange}
+                    className="form-input"
+                />
+                <button onClick={handleUrlSubmit} className="form-button">Submit URL</button>
+            </div>
         </div>
     );
 };
@@ -68,17 +80,20 @@ const Results = ({ data }) => {
     return (
         <div className="results-section">
             {data && data.hash ? (
-                <div>
-                    <p>Message: {data.message}</p>
+                <div className="message-container">
+                    <p className="message">Message: {data.message}</p>
                 </div>
             ) : (
-                <p>No data available</p>
+                <div className="no-data-container">
+                    <p className="no-data-message">No data available</p>
+                </div>
             )}
         </div>
     );
 };
 
-const Collocations = ({ data }) => {
+
+const Collocations = ({data}) => {
     if (!Array.isArray(data)) {
         return <p>No collocations available</p>;
     }
@@ -100,9 +115,13 @@ const Collocations = ({ data }) => {
 };
 
 const Grammar = ({ data }) => {
-    if (!Array.isArray(data)) {
-        return <p>No grammar exercises available</p>;
-    }
+    const [userInputs, setUserInputs] = useState(data.map(() => ''));
+
+    const handleInputChange = (index, value) => {
+        const newInputs = [...userInputs];
+        newInputs[index] = value;
+        setUserInputs(newInputs);
+    };
 
     return (
         <div className="grammar-section">
@@ -112,7 +131,14 @@ const Grammar = ({ data }) => {
                     <li key={index}>
                         <p>{item.sentence}</p>
                         <p>Исходная форма глагола: {item.raw_verb}</p>
-                        <p>Правильная форма глагола: {item.ready_verb}</p>
+                        <input
+                            type="text"
+                            value={userInputs[index]}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
+                            style={{
+                                backgroundColor: userInputs[index].toLowerCase() === item.ready_verb.toLowerCase() ? 'lightgreen' : 'white'
+                            }}
+                        />
                     </li>
                 ))}
             </ul>
@@ -173,7 +199,7 @@ const App = () => {
         <Router>
             <div className="App">
                 <header className="App-header">
-                    <h2>Trinity</h2>
+                    <h2 style={{color: '#61dafb', fontStyle: 'italic'}}>Trinity</h2>
                 </header>
                 <div className="input-section">
                     <Routes>
